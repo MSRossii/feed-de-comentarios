32990ce8549e74f23d2785bbd3ce44319ef2d860
MSRossii <marcelosrossi13@gmail.com>
2024-05-28 01:13:33 -0300

    create form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 export default function App() {
   return (
-    <>
-      <h1>Feed de Comentários</h1>
-    </>
+    <div id="app">
+      <h2>Feed de comentários</h2>
+      <form>
+        <label htmlFor="author">Email</label>
+        <input type="email" id="author" required />
+        <label htmlFor="content">Comentário</label>
+        <textarea id="content" cols="30" rows="6"></textarea>
+        <button type="submit">Enviar comentário</button>
+      </form>
+    </div>
   );
 }
