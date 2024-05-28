3ff6c8eef21b74c146b9f38f852ed9b3322158f2
MSRossii <marcelosrossi13@gmail.com>
2024-05-28 01:18:53 -0300

    states add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+
 export default function App() {
+  const [author, setAuthor] = useState("");
+  const [content, setContent] = useState("");
+
   return (
     <div id="app">
       <h2>Feed de comentários</h2>
       <form>
         <label htmlFor="author">Email</label>
-        <input type="email" id="author" required />
+        <input
+          type="email"
+          id="author"
+          required
+          value={author}
+          onChange={(ev) => setAuthor(ev.target.value)}
+        />
         <label htmlFor="content">Comentário</label>
         <textarea id="content" cols="30" rows="6"></textarea>
         <button type="submit">Enviar comentário</button>
